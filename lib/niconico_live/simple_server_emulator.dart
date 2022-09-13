9bc3b3cfd0bc888185756d9c5ca20ea27b94ee18
aoirint <aoirint@gmail.com>
2022-09-14 05:52:24 +0900

    user page client

--- lib/niconico_live/simple_server_emulator.dart
+++ lib/niconico_live/simple_server_emulator.dart
@@ -2,11 +2,13 @@ import 'package:logging/logging.dart';
 import 'live_page_server_emulator.dart';
 import 'watch_server_emulator.dart';
 import 'comment_server_emulator.dart';
+import 'user_page_server_emulator.dart';
 
 class NiconicoLiveSimpleServerEmulator {
   NiconicoLivePageServerEmulator? livePageServer;
   NiconicoLiveWatchServerEmulator? watchServer;
   NiconicoLiveCommentServerEmulator? commentServer;
+  NiconicoUserPageServerEmulator? userPageServer;
 
   late Logger logger;
 
@@ -26,11 +28,16 @@ class NiconicoLiveSimpleServerEmulator {
     final commentServer = NiconicoLiveCommentServerEmulator();
     await commentServer.start('127.0.0.1', 10082);
     this.commentServer = commentServer;
+
+    final userPageServer = NiconicoUserPageServerEmulator();
+    await userPageServer.start('127.0.0.1', 10083);
+    this.userPageServer = userPageServer;
   }
 
   Future<void> stop() async {
     commentServer?.stop();
     watchServer?.stop();
     livePageServer?.stop();
+    userPageServer?.stop();
   }
 }
